014d450aee03354f981452d010f9a91a300dd992
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-11-25 19:46:20 -0800

    dependencyBranch -> development

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 private let name: String = "SPFKSoundAnalysis" // Swift target
 private let dependencyNames: [String] = ["SPFKBase", "SPFKAudioBase", "SPFKTesting"]
-private let dependencyBranch: String = "main"
+private let dependencyBranch: String = "development"
 private let useLocalDependencies: Bool = false
 private let platforms: [PackageDescription.SupportedPlatform]? = [
     .macOS(.v12),
