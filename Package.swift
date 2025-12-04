665847f2dedcae990f5abff6f10379cc670d90f7
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-12-03 19:41:30 -0800

    renaming in progress

--- Package.swift
+++ Package.swift
@@ -5,11 +5,11 @@ import PackageDescription
 
 let name: String = "SPFKSoundAnalysis" // Swift target
 var localDependencies: [RemoteDependency] { [
-    .init(package: .package(url: "\(githubBase)/SPFKBase", from: "0.0.1"),
+    .init(package: .package(url: "\(githubBase)/spfk-base", from: "0.0.1"),
           product: .product(name: "SPFKBase", package: "spfk-base")),
-    .init(package: .package(url: "\(githubBase)/SPFKAudioBase", from: "0.0.1"),
+    .init(package: .package(url: "\(githubBase)/spfk-audio-base", from: "0.0.1"),
           product: .product(name: "SPFKAudioBase", package: "spfk-audio-base")),
-    .init(package: .package(url: "\(githubBase)/SPFKTesting", from: "0.0.1"),
+    .init(package: .package(url: "\(githubBase)/spfk-testing", from: "0.0.1"),
           product: .product(name: "SPFKTesting", package: "spfk-testing")),
 ] }
 let remoteDependencies: [RemoteDependency] = []
