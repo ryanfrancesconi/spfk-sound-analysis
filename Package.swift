ed0a59cc146ff1b93fe13b3467fd438bec24485e
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-02-21 08:59:19 -0800

    package versions

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/ryanfrancesconi/spfk-base", from: "0.0.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-base", from: "0.0.3"),
         .package(url: "https://github.com/ryanfrancesconi/spfk-audio-base", from: "0.0.1"),
         .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "0.0.1"),
     ],
